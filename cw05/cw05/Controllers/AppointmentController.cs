d33bf2bf5a916cf4d6adbbeeeb8f06ab47c270c4
s27800 <s27800@pjwstk.edu.pl>
2024-04-08 19:05:01 +0200

    Finished

---
+++ cw05/cw05/Controllers/AppointmentController.cs
@@ -0,0 +1,36 @@
+﻿using cw05.Database;
+using cw05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw05.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AppointmentController : ControllerBase
+{
+    [HttpGet("{id:int}")]
+    public IActionResult GetAppointmentsForAnimal(int id)
+    {
+        var animal = AnimalsDB.Animals.FirstOrDefault(a => a.Id == id);
+        if (animal == null)
+            return NotFound($"Animal with id={id} was not found");
+        
+        return Ok(AppointmentDB.GetAppointmentsForAnimal(animal));
+    }
+
+    [HttpPost]
+    public IActionResult CreateAppointment(Appointment appointment)
+    {
+        var animal = AnimalsDB.Animals.FirstOrDefault(a => a.Id == appointment.Animal1.Id);
+        if (animal == null)
+            AnimalsDB.Animals.Add(appointment.Animal1);
+        else if (animal != appointment.Animal1)
+        {
+            AnimalsDB.Animals.Remove(animal);
+            AnimalsDB.Animals.Add(appointment.Animal1);
+        }
+        
+        AppointmentDB.Appointments.Add(appointment);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+}
--- cw05/cw05/Database/AppointmentDB.cs
+++ cw05/cw05/Database/AppointmentDB.cs
@@ -5,4 +5,17 @@ namespace cw05.Database;
 public class AppointmentDB
 {
     public static List<Appointment> Appointments { get; set; } = new();
+
+    public static List<Appointment> GetAppointmentsForAnimal(Animal animal)
+    {
+        List<Appointment> list = new();
+        
+        foreach (var appointment in Appointments)
+        {
+            if(appointment.Animal1 == animal)
+                list.Add(appointment);
+        }
+
+        return list;
+    }
 }
